30438052862e4af5e02ea862e20cee2726120be7
ola <olanazmy@gmail.com>
2025-06-05 04:10:11 +0300

    useEffect function test

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
-import {  useState } from 'react';
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
-export default function App() 
-{
+export default function App() {
   // state in top level
-  const[advice, setAdvice] = useState("");
-  const[count,setCount] = useState(0);
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
   // function to get data
-  async function getAdvice()
-  {
-    const res  = await fetch("https://api.adviceslip.com/advice");
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
     setAdvice(data.slip.advice);
-    setCount((c) => c+1);
+    setCount((c) => c + 1);
   }
 
+  useEffect(function () {
+    getAdvice();
+  },[]);
 
   return (
     <>
-    <div> 
-    <h2>{advice}</h2>
-    <button onClick={getAdvice}>Get advice</button>
-    <p>you have read <strong>{count}</strong> pieces of advice</p>
-
-    </div>
+      <div>
+        <h2>{advice}</h2>
+        <button onClick={getAdvice}>Get advice</button>
+        <p>
+          you have read <strong>{count}</strong> pieces of advice
+        </p>
+      </div>
     </>
-  )
+  );
 }
-
