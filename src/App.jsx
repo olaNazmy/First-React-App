3e2c4849f9720fae3ef632a57e90c360ba7d7e5d
ola <olanazmy@gmail.com>
2025-06-05 04:05:04 +0300

    adding count for advices

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,23 @@ export default function App()
 {
   // state in top level
   const[advice, setAdvice] = useState("");
+  const[count,setCount] = useState(0);
   // function to get data
   async function getAdvice()
   {
     const res  = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
     setAdvice(data.slip.advice);
+    setCount((c) => c+1);
   }
 
 
   return (
     <>
-    <div><h1>hello i am ola</h1>
+    <div> 
     <h2>{advice}</h2>
     <button onClick={getAdvice}>Get advice</button>
+    <p>you have read <strong>{count}</strong> pieces of advice</p>
 
     </div>
     </>
